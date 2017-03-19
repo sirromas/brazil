47b5b616c1c5d11a21d2aa94f1bba486756f50d9
sirromas <sirromas@gmail.com>
2017-03-19 18:01:30 +0200

    Apply PayPal fix

---
+++ test_paypal.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+<br/><br/><br/>
+<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
+        <input type="hidden" name="cmd" value="_xclick"><input type="hidden" name="charset" value="utf-8"><input type="hidden" name="business" value="sirromas@ukr.net"><input type="hidden" name="item_name" value="Curso de Reciclagem para Condutores Infratores">
+        <input type="hidden" name="amount" value="270">
+        <input type="hidden" name="custom" value="sirromas@gmail.com/Greenmoodle123#/754135243-52/1">    
+        <input type="hidden" name="currency_code" value="BRL">    
+        <input type="hidden" name="first_name" value="">
+        <input type="hidden" name="last_name" value="">
+        <input type="hidden" name="address1" value="Rua Aroazes 691">
+        <input type="hidden" name="city" value="Jacarepagua">
+        <input type="hidden" name="state" value="">
+        <input type="hidden" name="zip" value="">
+        <input type="hidden" name="night_phone_a" value="">
+        <input type="hidden" name="night_phone_b" value="">
+        <input type="hidden" name="email" value="sirromas@gmail.com">
+        <input type="hidden" name="return" value="http://ead.iprovida.org.br/custom/signup/paypal_success.php">
+        <input type="image" name="submit" src="https://www.iccjp.com/wp-content/uploads/2014/08/pagar-com-paypal.png" width="352" height="52" alt="PayPal - The safer, easier way to pay online">
+      </form>
